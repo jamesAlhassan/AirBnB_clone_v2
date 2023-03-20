0552c381e686beec2134d5d01ee46eb6b5c4aa37
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-20 22:25:40 +0000

    Move the new(self) to init

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.save()
         else:
             from models import storage
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
@@ -32,7 +31,6 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-            self.save()
 
     def __str__(self):
         """Returns a string representation of the instance"""
