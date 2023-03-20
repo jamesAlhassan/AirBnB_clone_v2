618e1e20158e49c7bb2b851d1cfe2001bfa977ff
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-20 22:19:34 +0000

    Use the right format for datetime

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"))
-    updated_at = Column(DateTime, nullable=False, server_default=text
-            ("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow()) 
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
