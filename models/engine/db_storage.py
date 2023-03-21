95e37a91c7bc3994f4bfaa95a2d9386b5c2e7afd
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-21 10:16:31 +0000

    Handle errors of data loading

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -60,7 +60,7 @@ class DBStorage:
     def reload(self):
         """create all tables of dtabase"""
 
-        Base.metadata.create_self(self.__engine)
+        Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
