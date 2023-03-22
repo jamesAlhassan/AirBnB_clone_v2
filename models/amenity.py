a913c6431e386382649fb063a6f15dfd8aef6e30
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-22 19:00:43 +0000

    Clean up code

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models import type_storage
 
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity Class"""
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("amenity", secondary="place_amenity")
+
+    if type_storage == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
