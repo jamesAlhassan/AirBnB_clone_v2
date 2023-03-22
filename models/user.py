0f29bccaaeaa0657763c9e54fb7b5987433ba913
James Alhassan <jamesalhassan052@gmail.com>
2023-03-22 09:31:27 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -17,8 +17,9 @@ class User(BaseModel, Base):
     places = relationship("Place", backref="user",
                           cascade="all, delete-orphan")
     """reviews = relationship("Review", backref="user",
-                           cascade="all, delete-orphan")"""
+                           cascade="all, delete-orphan")
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+    """
