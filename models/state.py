394a101d85881bbfb05c00f6f96d31cb6f8672db
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-22 18:35:48 +0000

    Clean up code

--- models/state.py
+++ models/state.py
@@ -4,12 +4,16 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import type_storage
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete",
-                              backref="states")
+    if type_storage == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete,
+                              delete-orphan", backref="state")
+    else:
+        name = ''
