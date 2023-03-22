6bad027c035501ef3eb7aaaa8b17ac81473d24e9
James Alhassan <jamesalhassan052@gmail.com>
2023-03-22 11:18:33 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class Amenity(BaseModel, Base):
     """Representation of Amenity Class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(Amenity,
-                    secondary=place_amenity)
+    place_amenities = relationship("amenity", secondary="place_amenity")
