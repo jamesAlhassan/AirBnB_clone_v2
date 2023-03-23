55889acd1fbeb65a21378e6cad773e2b9d69b004
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-23 00:50:35 +0000

    Add getter method

--- models/state.py
+++ models/state.py
@@ -4,16 +4,30 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models import type_storage
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    if type_storage == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete,
-                              delete-orphan", backref="state")
+        cities = relationship("City", backref='state',
+                              cascade="all, delete, delete-orphan")
     else:
         name = ''
+
+        @property
+        def cities(self):
+            """
+            fs getter attribute that returns City instances
+            state_id equals the current state
+            """
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
