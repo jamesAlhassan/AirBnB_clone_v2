2f2d2a8fa9c29420f60e03a0bb8842087b15060d
James Alhassan <jamesalhassan052@gmail.com>
2023-04-06 17:24:09 +0000

    Add 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#generates a .tgz archive from the contents of the web_static folder
+
+from fabric.api import *
+from time import strftime as st
+from datetime import datetime as dt
+def do_pack():
+''' archive generator func'''
+    now = dt.now()
+    name = now.st('%Y%m%d%H%M%S')
+
+    local('mkdir -p versions')
+
