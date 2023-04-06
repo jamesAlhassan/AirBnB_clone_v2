b2621568599d15ee4a7011f45d789356a72b35cc
James Alhassan <jamesalhassan052@gmail.com>
2023-04-06 16:31:31 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,3 +5,10 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow "Nginx HTTP"
+
+# create dirs
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
