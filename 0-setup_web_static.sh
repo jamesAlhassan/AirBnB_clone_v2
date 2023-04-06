9b46ef625aaf26d1c33294798f9b2fd765acbfc7
James Alhassan <jamesalhassan052@gmail.com>
2023-04-06 16:30:48 +0000

    Add 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,7 @@
 #!/usr/bin/env bash
-# script sets up web servers for deployment of web_static
+#Sets up your web servers for the deployment of web_stati
 
-# Check for nginx. If not installed, install and configure
-sudo apt-get update;
-sudo apt-get -y install nginx;
-sudo ufw allow 'Nginx HTTP'
-
-# check and create directories
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-# create index.html file and add html content
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# create symbolic link
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-# give ownership of data to ubuntu
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
+#package repo update, install nginx & firewall config
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow "Nginx HTTP"
