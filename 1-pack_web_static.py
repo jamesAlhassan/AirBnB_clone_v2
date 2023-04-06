818e50865572de244b849819eb5b7954fb5ba7ff
James Alhassan <jamesalhassan052@gmail.com>
2023-04-06 22:14:19 +0000

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,8 @@ def do_pack():
     ''' archive generator func'''
     local("sudo mkdir -p versions")
     date = dt.now().strftime("%Y%m%d%H%M%S")
-    filename = f"versions/web_static_{date}.tgz"
-    result = local("sudo tar -cvzf {filename} web_static")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
     if result.succeeded:
         return filename
     else:
