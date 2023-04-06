2293c17435c842272a7bf8b2c2f1a6ec897d6bb2
James Alhassan <jamesalhassan052@gmail.com>
2023-04-06 16:44:41 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,3 +28,9 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 #change ownership to ubuntu
 sudo chown -R ubuntu:ubuntu /data/
+
+#default config
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+#restart server
+sudo service nginx restart
