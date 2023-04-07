572a92dc7d695a50de7b158786f6bd96560fbc8d
James Alhassan <jamesalhassan052@gmail.com>
2023-04-07 05:57:53 +0000

    Update vi 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 '''distributes an archive to your web servers, using the function do_deploy:'''
 from fabric.api import put, run, env
-from datetime import datetime as dt
+from os.path import exists
+
 
 env.hosts = ["100.25.22.118", "52.90.13.68"]
 
 
 def do_deploy(archive_path):
     '''distributes an archive to your web servers, using do_deploy '''
-    if (not archive_path):
+    if exists(archive_path) is False:
         rerurn False
     file = (archive_path.split('/')[-1]).split('.')[0]
     path = '/data/web_static/releases/' + "{}".format(file)
     tmp = '/tmp/' + file
 
-    put(archive_path, "/tmp/")
-    run("mkdir -p {}/".format(path))
-    run("tar -xzf {} -C {}/".format(tmp, path))
-    run("rm {}".format(tmp))
-    run("mv {}/web_static/* {}/".format(path, path))
-    run("rm -rf {}/web_static".format(path))
-    run("rm -rf /data/web_static/current")
-    run("ln -s {}/ /data/web_static/current".format(path))
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}/".format(path))
+        run("sudo tar -xzf {} -C {}/".format(tmp, path))
+        run("sudo rm {}".format(tmp))
+        run("sudo mv {}/web_static/* {}/".format(path, path))
+        run("sudo rm -rf {}/web_static".format(path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}/ /data/web_static/current".format(path))
+
+        return True
+
+    except:
+        return False
