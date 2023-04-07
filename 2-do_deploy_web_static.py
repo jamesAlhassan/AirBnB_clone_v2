f8ef739e13bfb80329f6580e793075ca7d1f5bbb
James Alhassan <jamesalhassan052@gmail.com>
2023-04-07 05:46:56 +0000

    Add vi 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,7 +14,11 @@ def do_deploy(archive_path):
     path = '/data/web_static/releases/' + "{}".format(file)
     tmp = '/tmp/' + file
 
-    put(archive_path, '/tmp/')
-    run('mkdir -p {}/'.format(path))
-    run('tar -xzf {} -C {}/'.format(tmp, path))
-    run('rm {}'.format(tmp))
+    put(archive_path, "/tmp/")
+    run("mkdir -p {}/".format(path))
+    run("tar -xzf {} -C {}/".format(tmp, path))
+    run("rm {}".format(tmp))
+    run("mv {}/web_static/* {}/".format(path, path))
+    run("rm -rf {}/web_static".format(path))
+    run("rm -rf /data/web_static/current")
+    run("ln -s {}/ /data/web_static/current".format(path))
