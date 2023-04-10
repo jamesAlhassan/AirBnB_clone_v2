a075bbb74a397ec28b4e1b7e92a43426a9ec8b48
James Alhassan <jamesalhassan052@gmail.com>
2023-04-10 05:51:04 +0000

    Add 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -18,3 +18,33 @@ $nginx_conf = "server {
       internal;
     }
 }"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+}
+
+-> file { '/data':
+  ensure  => 'directory'
+}
+
+-> file { '/data/web_static':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
+}
