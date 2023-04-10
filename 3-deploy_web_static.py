256ff7330604bdda713df01079bbe993687e133c
James Alhassan <jamesalhassan052@gmail.com>
2023-04-10 04:30:06 +0000

    Upate  3-do_deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os.path
-from datetime import datetime
+from datetime import datetime as d
 from fabric.api import env
 from fabric.api import local
 from fabric.api import put
@@ -8,3 +8,21 @@ from fabric.api import run
 
 env.hosts = ["100.25.22.118", "52.90.13.68"]
 
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = d.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+
+    return file
