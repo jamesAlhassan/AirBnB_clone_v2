8dafb8e2a050e1cbf2beb0f969d644bd9faf9620
James Alhassan <jamesalhassan052@gmail.com>
2023-04-10 05:55:21 +0000

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,7 +10,7 @@ $nginx_conf = "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://linktr.ee/firdaus_h_salim/;
+        return 301 http://github.com/;
     }
     error_page 404 /404.html;
     location /404 {
