67763beae065d38f47e1dc925577d9617a79f6c2
James Alhassan <jamesalhassan052@gmail.com>
2023-04-10 05:45:10 +0000

    Update  100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,4 +4,11 @@ from fabric.api import *
 
 env.hosts = ["100.25.22.118", "52.90.13.68"]
 
+def do_clean(number=0):
+    ''' Remove old archives '''
+    number = 1 if int(number) == 0 else int(number)
 
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
