ad425e05e9c80313958a906e2dd579672d82720e
James Alhassan <jamesalhassan052@gmail.com>
2023-04-22 08:27:13 +0000

    Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,35 +12,52 @@ env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Deploy web files to remote server"""
+    """
+    Distributes an archive to webservers
+    Arguments:
+        archive_path: path of the archive file
+    Return:
+        False if file does not exist at path
+        True if all operations have been done
+    """
+    # check if file exists
     try:
         if not (path.exists(archive_path)):
             return False
 
+        # upload the archive to the /tmp/ directory of the server
         put(archive_path, '/tmp/')
 
+        # get timestamp and use to create target dir
         timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
+        run("sudo mkdir -p /data/web_static/releases/web_static_{}/"
             .format(timestamp))
 
-        run('sudo rm -rf /data/web_static/current')
+        # Uncompress the archive
+        run('sudo tar -xzf  /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}'
+            .format(timestamp, timestamp))
 
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except Exception:
-            return False
+        # Delete the archive from the web server
+        run("sudo rm /tmp/web_static_{}.tgz".format(timestamp))
 
-        return True
+        # move contents to the host
+        run("sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+                /data/web_static/releases/web_static_{}"
+            .format(timestamp, timestamp))
+
+        # delete useless dirs
+        run("sudo rm -rf /data/web_static/releases/web_static_{}/web_static"
+            .format(timestamp))
+
+        # Delete the symbolic link from the web server
+        run("sudo rm -rf /data/web_static/current")
+
+        # create new symbolic link on the web server
+        run("sudo ln -s /data/web_static/releases/web_static_{}/ \
+            /data/web_static/current".format(timestamp))
+    except Exception:
+        return False
+
+    # return after everything runs successfully
+    return True
