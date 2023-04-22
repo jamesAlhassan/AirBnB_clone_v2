4d8316080095e2da39967ec32937d000c66007ea
James Alhassan <jamesalhassan052@gmail.com>
2023-04-22 08:22:13 +0000

    Upate 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,36 +12,35 @@ env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-        """Deploy web files to remote server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
+    """Deploy web files to remote server"""
+    try:
+        if not (path.exists(archive_path)):
+            return False
 
-                put(archive_path, '/tmp/')
+        put(archive_path, '/tmp/')
 
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
 releases/web_static_{}/'.format(timestamp))
 
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
 /data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
+            .format(timestamp, timestamp))
 
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
 /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
-                run('sudo rm -rf /data/web_static/releases/\
+        run('sudo rm -rf /data/web_static/releases/\
 web_static_{}/web_static'
-                    .format(timestamp))
+            .format(timestamp))
 
-                run('sudo rm -rf /data/web_static/current')
+        run('sudo rm -rf /data/web_static/current')
 
-                run('sudo ln -s /data/web_static/releases/\
+        run('sudo ln -s /data/web_static/releases/\
 web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
+        except Exception:
+            return False
 
         return True
