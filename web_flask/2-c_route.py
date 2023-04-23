1bca39f9db8f4cb61ebe4d0872d2af977e116624
James Alhassan <jamesalhassan052@gmail.com>
2023-04-23 15:22:06 +0000

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,8 +23,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     '''Displays C with input "text" on /c/text'''
-    return (f"C {text}".replace("_", " "))
-
+    return f"C {text}".replace("_", " ")
 
 
 if __name__ == "__main__":
