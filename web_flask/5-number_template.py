82088930fb9c8912ddc34fb7f9cfae6d607bef1e
James Alhassan <jamesalhassan052@gmail.com>
2023-04-23 22:29:13 +0000

    Update 5-number.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,11 +40,11 @@ def isNum(n):
         return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>")
 def numTemplate(n=None):
     '''Renders template "5-number.html" if n is an int'''
     if isinstance(n, int):
-        return render_template("5-number.html", n=n)
+        return render_template("5-number.html", num=n)
 
 
 if __name__ == "__main__":
