0ddcef437f9cd881c8f7add87e32cb6d9c4dbde7
James Alhassan <jamesalhassan052@gmail.com>
2023-04-23 15:15:07 +0000

    Add 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' starts a Flask web application
+with host "0.0.0.0" and port "5000"
+'''
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def greet():
+    ''' Greets Hello HBNB! on /'''
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''Displays hbnb on route /hbnb'''
+    return ("HBNB")
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    '''Displays C with input "text" on /c/text'''
+    return (f"C {text}".replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
