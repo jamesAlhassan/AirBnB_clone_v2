52be138c2f3700d7bc1ecc28278ff48f34a16b56
James Alhassan <jamesalhassan052@gmail.com>
2023-04-27 05:32:55 +0000

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,28 +6,28 @@ with host "0.0.0.0" and port "5000"
 from flask import Flask, render_template
 
 app = Flask("__name__")
+app.url_map.strict_slashes = False
 
-
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def greet():
     ''' Greets Hello HBNB! on /'''
     return ("Hello HBNB!")
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     '''Displays hbnb on route /hbnb'''
     return ("HBNB")
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c_text(text):
     '''Displays C with input "text" on /c/text'''
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python")
+@app.route("/python/<text>")
 def python_text(text="is cool"):
     '''Displays Python with "text" on /python/text'''
     return "Python {}".format(text.replace("_", " "))
@@ -40,7 +40,7 @@ def isNum(n):
         return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>")
 def numTemplate(n=None):
     '''Renders template "5-number.html" if n is an int'''
     if isinstance(n, int):
@@ -48,4 +48,4 @@ def numTemplate(n=None):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
