bed769808ff6514f63deea24da8c9ea2fbd65b98
James Alhassan <jamesalhassan052@gmail.com>
2023-04-27 05:37:13 +0000

    Update #!/usr/bin/python3

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,52 +1,53 @@
 #!/usr/bin/python3
-''' starts a Flask web application
-with host "0.0.0.0" and port "5000"
-'''
-
+"""Starts Flask web app listening on 0.0.0.0, port 5000"""
 from flask import Flask, render_template
 
-app = Flask("__name__")
+
+app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def greet():
-    ''' Greets Hello HBNB! on /'''
-    return ("Hello HBNB!")
+@app.route('/')
+def hello():
+    """route to print out a text"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb")
+@app.route('/hbnb')
 def hbnb():
-    '''Displays hbnb on route /hbnb'''
-    return ("HBNB")
+    """displays HBNB"""
+    return "HBNB"
 
 
 @app.route("/c/<text>")
 def c_text(text):
-    '''Displays C with input "text" on /c/text'''
+    """Displays "C" followed by the value of text"""
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route("/python")
 @app.route("/python/<text>")
 def python_text(text="is cool"):
-    '''Displays Python with "text" on /python/text'''
+    """
+    Displays 'Python followed by the value of text variable
+    Default value of text is 'is cool'
+    """
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route("/number/<int:n>")
-def isNum(n):
-    '''Displays "n" is a number if n is an int passed on /number/n'''
+def display_n(n):
+    """Displays 'n is a number' only if n is an integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>")
-def numTemplate(n=None):
-    '''Renders template "5-number.html" if n is an int'''
+def display_html(n=None):
+    """Displays a HTML page only if n is an integer"""
     if isinstance(n, int):
         return render_template("5-number.html", n=n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
